ab4e2662bc86be08718bc3804a4d89cb8970c36a
Liam MacPherson <liammacpherson1996@gmail.com>
2024-01-18 09:14:48 +0000

    LZA-115: add repositories that we support
    To ensure that we are working in the open, this change adds a generic
    support page that includes the repositories that are actively maintained
    by the team.

---
+++ documentation/support/index.11tydata.js
@@ -0,0 +1,29 @@
+module.exports = {
+    actionRepos: [
+        {
+            name: "core-cloud-add-customer-action",
+            url: "https://www.github.com/UKHomeOffice/core-cloud-add-customer-action"
+        },
+        {
+            name: "match-label-action",
+            url: "https://www.github.com/UKHomeOffice/match-label-action"
+        },
+        {
+            name: "semver-calculate-action",
+            url: "https://www.github.com/UKHomeOffice/semver-calculate-action"
+        },
+        {
+            name: "semver-tag-action",
+            url: "https://www.github.com/UKHomeOffice/semver-tag-action"
+        },
+    ],
+    documentationRepos: [{
+        name: "core-cloud",
+        url: "https://www.github.com/UKHomeOffice/core-cloud"
+    },], configurationRepos: [
+        {
+            name: "core-cloud-github-config",
+            url: "https://www.github.com/UKHomeOffice/core-cloud-github-config"
+        },
+    ],
+}
